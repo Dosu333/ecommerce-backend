964c1d092f59a904dff6facdbab3e71192a3c12f
Dosu333 <vladamir1865@gmail.com>
2023-07-18 23:35:22 +0100

    refactor

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-require('dotenv/config');
+
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const Product = require('./models/product')
+const productsRouter = require('./routers/product')
+
+// Environment variables
+require('dotenv/config');
 const api = process.env.API_URL;
 const connectionString = process.env.CONNECTION_STRING
 
@@ -11,45 +16,8 @@ const connectionString = process.env.CONNECTION_STRING
 app.use(bodyParser.json());
 app.use(morgan('tiny'))
 
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: Number
-})
-
-const Product = mongoose.model('Product', productSchema);
-
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: 'hairdresser',
-        image: 'some_url'
-    };
-
-    res.send(product);
-})
-
-app.post(`${api}/products`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-
-    product.save()
-    .then((createdProduct => {
-        res.status(201).json({
-            success: true,
-            product: createdProduct
-        })
-    }))
-    .catch((err) => {
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
-})
+// Routers
+app.use(`${api}/products`, productsRouter)
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
