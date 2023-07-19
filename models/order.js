ba710fb719a5df666ec4e6e25252b850b5e90061
Dosu333 <vladamir1865@gmail.com>
2023-07-19 04:15:37 +0100

    order model

---
+++ models/order.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const OrderItem = require('./orderitem');
+
+const orderSchema = mongoose.Schema({
+    orderitems: [OrderItem],
+    shippingAddress1: String,
+    shippingAddress2: String,
+    city: String,
+    zip: String,
+    country: String,
+    phone: Number,
+    status: String,
+    totalPrice: Number,
+    user: User,
+    dateOrdered: Date
+})
+
+exports.Order = mongoose.model('Order', orderSchema)
