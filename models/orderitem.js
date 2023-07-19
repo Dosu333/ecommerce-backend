88f382f1fbe21061faf77705ad28f530658499cd
Dosu333 <vladamir1865@gmail.com>
2023-07-19 04:07:22 +0100

    orderitem model

---
+++ models/orderitem.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Product = require('./product');
+
+const orderItemSchema = mongoose.Schema({
+    product: Product,
+    quantity: {
+        type: Number,
+        default: 0
+    }
+})
+
+exports.OrderItem = mongoose.model('OrderItem', orderItemSchema)
