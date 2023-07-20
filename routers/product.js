838a3d4f0f81fbe62597b25291fbdf1fc39916bf
Dosu333 <vladamir1865@gmail.com>
2023-07-20 12:09:12 +0100

    refactor product list and detail

--- routers/product.js
+++ routers/product.js
@@ -4,7 +4,7 @@ const { Category } = require('../models/category');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const productList = await Product.find().select('name image -_id');
+    const productList = await Product.find().select('name image category -_id').populate('category');
     if (!productList) {
         return res.status(500).json({
             success: false
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
-    const product = await Product.findById(req.params.id)
+    const product = await Product.findById(req.params.id).populate('category')
 
     if (!product) {
         return res.status(404).json({
