62b8c4acc0ed1f8ec0de9fedfde253ef8b83347f
Dosu333 <vladamir1865@gmail.com>
2023-07-20 16:46:15 +0100

    user schema

--- models/user.js
+++ models/user.js
@@ -13,13 +13,43 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    street: String,
-    apartment: String,
-    city: String,
-    zip: String,
-    country: String,
-    phone: String,
-    isAdmin: Boolean
+    phone: {
+        type: String,
+        required: true
+    },
+    street: {
+        type: String,
+        default: ''
+    },
+    apartment: {
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    zip: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: ''
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
 })
 
-exports.User = mongoose.model('User', userSchema)
+userSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+userSchema.set('toJSON', {
+    virtuals: true,
+});
+
+exports.User = mongoose.model('User', userSchema);
+exports.userSchema = userSchema;
